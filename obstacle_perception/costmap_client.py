8a3c016a073c2850137828b1f50a809c42033de3
Pito Salas <pitosalas@gmail.com>
2025-04-04 20:32:49 -0400

    work in progress

--- obstacle_perception/costmap_client.py
+++ obstacle_perception/costmap_client.py
@@ -17,3 +17,22 @@ class CostmapClient(Node):
 
     def get_costmap(self):
         return self.latest_costmap
+
+def main(args=None):
+    import rclpy
+    rclpy.init(args=args)
+    node = CostmapClient()
+
+    def print_costmap():
+        cm = node.get_costmap()
+        if cm:
+            node.get_logger().info(f"Received costmap: {cm.metadata.size_x} x {cm.metadata.size_y}")
+        else:
+            node.get_logger().info("Waiting for costmap...")
+
+    node.create_timer(1.0, print_costmap)
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -2,29 +2,32 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'obstacle_perception'
+package_name = "obstacle_perception"
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version="0.0.1",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'msg'), glob('msg/*.msg')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "msg"), glob("msg/*.msg")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.yaml")),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Pito Salas',  # Change this to your name
-    maintainer_email='pitosalas@gmail.com',
-    description='Obstacle perception using costmap and beam checking.',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Pito Salas",  # Change this to your name
+    maintainer_email="pitosalas@gmail.com",
+    description="Obstacle perception using costmap and beam checking.",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'perception_node = obstacle_perception.perception_node:main'
+        "console_scripts": [
+            "perception_node = obstacle_perception.perception_node:main",
+            "costmap_client = obstacle_perception.costmap_client:main",
         ],
     },
 )
-
